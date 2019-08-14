f993b3dbb8454d37abd2545f2e54cd4502656a57
Saransh Sharma <marktwain@gmx.us>
2019-08-14 20:19:01 +0530

    first commit

---
+++ src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {PfApi} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new PfApi({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: PfApi;
+  client: Client;
+}
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {PfApi} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {PfApi};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new PfApi(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
